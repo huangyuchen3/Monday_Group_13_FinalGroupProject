03cf9a607dde1f1649617de0fa359efea28cf347
huangyuchen3 <yuchen@yuchendeMacBook-Pro.local>
2024-11-27 16:45:56 -0500

    modify forms

--- SustainableAccessoryMarket/src/Business/Role/PartnerDonorRole.java
+++ SustainableAccessoryMarket/src/Business/Role/PartnerDonorRole.java
@@ -1,4 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Business.Role;
 
-public class PartnerDonorRole {
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import ui.DonorRole.DonorAreaJPanel;
+
+/**
+ *
+ * @author reetikabhanushali 
+ */
+public class PartnerDonorRole extends Role{
+    
+     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business, JPanel jp, JSplitPane jsp) {
+        return new DonorAreaJPanel(userProcessContainer, business, account,jp,jsp);
+    }
+    
 }
