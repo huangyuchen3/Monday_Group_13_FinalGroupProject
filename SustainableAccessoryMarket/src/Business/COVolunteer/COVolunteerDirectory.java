9cd3678a873f90a710e39f0d79835672d72892fc
huangyuchen3 <yuchen@yuchendeMacBook-Pro.local>
2024-12-01 17:27:03 -0500

    add volunteer

--- SustainableAccessoryMarket/src/Business/COVolunteer/COVolunteerDirectory.java
+++ SustainableAccessoryMarket/src/Business/COVolunteer/COVolunteerDirectory.java
@@ -4,7 +4,10 @@
  */
 package Business.COVolunteer;
 
+import Business.Role.Volunteer;
+import Business.UserAccount.UserAccount;
 import Business.UserAccount.UserAccountDirectory;
+import Business.Worker.Worker;
 import java.util.ArrayList;
 
 /**
@@ -42,7 +45,47 @@ public class COVolunteerDirectory {
     }
     
     
+//    public void initializeDefaultVolunteers(UserAccountDirectory userAccountDirectory) {
+//        COVolunteer volunteer1 = new COVolunteer();
+//        volunteer1.setVolId("V1");
+//        volunteer1.setVolName("Volunteer One");
+//        volunteer1.setVolAge("25");
+//        volunteer1.setVolPhno("1234567890");
+//        volunteer1.setVolAddress("123 Main St");
+//        volunteer1.setVolCity("Boston");
+//        volunteer1.setVolState("MA");
+//        volunteer1.setVolZipcode("02116");
+//        volunteer1.setVolExp("2");
+//        volunteer1.setManagerUsername("comanager1"); // Assign to comanager1
+//
+//        COVolunteer volunteer2 = new COVolunteer();
+//        volunteer2.setVolId("V2");
+//        volunteer2.setVolName("Volunteer Two");
+//        volunteer2.setVolAge("30");
+//        volunteer2.setVolPhno("9876543210");
+//        volunteer2.setVolAddress("456 Elm St");
+//        volunteer2.setVolCity("Cambridge");
+//        volunteer2.setVolState("MA");
+//        volunteer2.setVolZipcode("02140");
+//        volunteer2.setVolExp("5");
+//        volunteer2.setManagerUsername("comanager1"); // Assign to comanager1
+//
+//        this.volList.add(volunteer1);
+//        this.volList.add(volunteer2);
+//    }
+    
     public void initializeDefaultVolunteers(UserAccountDirectory userAccountDirectory) {
+        // Create Volunteer 1
+        Worker volunteerWorker1 = new Worker();
+        volunteerWorker1.setName("Volunteer One");
+
+        UserAccount volunteerAccount1 = userAccountDirectory.createUserAccount(
+            "volunteer1", // Default username
+            "@Password123",  // Default password
+            volunteerWorker1,
+            new Volunteer() // Assuming you have a role for volunteers
+        );
+
         COVolunteer volunteer1 = new COVolunteer();
         volunteer1.setVolId("V1");
         volunteer1.setVolName("Volunteer One");
@@ -54,6 +97,18 @@ public class COVolunteerDirectory {
         volunteer1.setVolZipcode("02116");
         volunteer1.setVolExp("2");
         volunteer1.setManagerUsername("comanager1"); // Assign to comanager1
+        volunteer1.setVolAccount(volunteerAccount1); // Link the UserAccount to the volunteer
+
+        // Create Volunteer 2
+        Worker volunteerWorker2 = new Worker();
+        volunteerWorker2.setName("Volunteer Two");
+
+        UserAccount volunteerAccount2 = userAccountDirectory.createUserAccount(
+            "volunteer2", // Default username
+            "@Password123",  // Default password
+            volunteerWorker2,
+            new Volunteer()
+        );
 
         COVolunteer volunteer2 = new COVolunteer();
         volunteer2.setVolId("V2");
@@ -66,8 +121,18 @@ public class COVolunteerDirectory {
         volunteer2.setVolZipcode("02140");
         volunteer2.setVolExp("5");
         volunteer2.setManagerUsername("comanager1"); // Assign to comanager1
+        volunteer2.setVolAccount(volunteerAccount2); // Link the UserAccount to the volunteer
 
+        // Add volunteers to the list
         this.volList.add(volunteer1);
         this.volList.add(volunteer2);
+
+        // Debug messages to confirm initialization
+        System.out.println("Default Volunteers initialized.");
+        for (COVolunteer vol : this.volList) {
+            System.out.println("ID: " + vol.getVolId() + ", Name: " + vol.getVolName()
+                    + ", Username: " + vol.getVolAccount().getUsername());
+        }
     }
+
 }
